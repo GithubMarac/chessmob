fdc2f1596c03d20a061debbe654fba108fcc906b
GithubMarac <mario.probst2@gmail.com>
2023-09-14 19:34:23 +0200

    added scan tab

---
+++ src/app/storage.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+
+import { Storage } from '@ionic/storage';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+  private _storage: Storage | null = null;
+
+  constructor(private storage: Storage) {
+    this.init();
+  }
+
+  async init() {
+    // If using, define drivers here: await this.storage.defineDriver(/*...*/);
+    const storage = await this.storage.create();
+    this._storage = storage;
+  }
+
+  // Create and expose methods that users of this service can
+  // call, for example:
+  public set(key: string, value: any) {
+    this._storage?.set(key, value);
+  }
+
+  public keys() {
+    this._storage?.keys();
+  }
+
+  public get(name: string) {
+    this._storage?.get(name);
+  }
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { StorageService } from '../storage.service';
+import { Camera, CameraResultType } from '@capacitor/camera';
+
+declare var Chessboard: any;
+
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +12,77 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  private board2: any
+  photo: any
 
-  constructor() {}
+  constructor(private toastController: ToastController,
+              private storageService: StorageService) { 
+
+  }
+
+  ngOnInit() {
+    
+  }
+
+  ionViewDidEnter() {
+    if(this.photo){
+      var ruyLopez = 'r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R'
+      this.board2 = Chessboard('board2', {
+        position: ruyLopez,
+        draggable: true,
+        dropOffBoard: 'trash',
+        sparePieces: true
+      });
+    }
+  }
+
+  async save() {
+    let date = new Date();
+    this.storageService.set(date.toISOString(), this.board2.fen());
+    const toast = await this.toastController.create({
+      message: "Position is saved.",
+      duration: 2000,
+      position: 'bottom',
+    });
+
+    await toast.present();
+  }
+
+
+  async analyze() {
+    const toast = await this.toastController.create({
+      message: this.board2.fen(),
+      duration: 2000,
+      position: 'bottom',
+    });
+
+    await toast.present();
+  }
+
+  takePicture(){
+    this.photo = async () => {
+      const image = await Camera.getPhoto({
+        quality: 90,
+        allowEditing: false,
+        resultType: CameraResultType.Uri
+      });
+    
+      // image.webPath will contain a path that can be set as an image src.
+      // You can access the original file using image.path, which can be
+      // passed to the Filesystem API to read the raw data of the image,
+      // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
+      var imageUrl = image.webPath;
+    
+      // Can be set to the src of an image now
+      //imageElement.src = imageUrl;
+    };
+  }
+
+  uploadPicture(){
+    
+  }
+
+
+
 
 }
